6695fb69d1134049a7983b4d4ebf6d820bed9d6d
kepha222 <kephamotari1@.com>
2022-11-27 13:39:10 +0300

    third commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -68,7 +68,7 @@ const Homepage = () => {
           Latest Crypto News
         </Title>
         <Title level={3} className="show-more">
-          <Link to="/cryptocurrencies">Show More</Link>
+          <Link to="/news">Show More</Link>
         </Title>
       </div>
       <News simplified />
